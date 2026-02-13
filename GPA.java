58cc1459612921e83b2a48fd1ff74f8b43f0e4bc
THARUSHA <tharushadilmin123@gmail.com>
2026-02-13 18:25:25 +0530

    Get couse count

---
+++ GPA.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GPA {
+
+    public static void main(String[] args){
+
+        System.out.println("How many course you had this semester");
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+
+        double[] nbcourses = new double[x];
+        
+
+        
+    }
+}
